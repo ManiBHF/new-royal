51bd7aaf6be07e8f109ea491999f9ea464f8f9cc
ManiBHF <manibeh84@gmail.com>
2023-08-05 13:29:40 +0330

    new update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,29 +4,29 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: '1rem',
+      padding: "1rem",
     },
     fontFamily: {
-      'danawt': 'danawt'
+      danawt: "danawt",
     },
     extend: {
       boxShadow: {
-        'mobile-menu': '0 0px 10px 0px rgba(0, 0, 0, .14);',
-        'country-card':  '0 2px 20px hsla(0,0%,65%,.342);',
+        "mobile-menu": "0 0px 10px 0px rgba(0, 0, 0, .14);",
+        "country-card": "0 2px 20px hsla(0,0%,65%,.342);",
       },
-    keyframes: {
-      cloud: {
-        '100%': {left : '100%'},
+      keyframes: {
+        cloud: {
+          "100%": { left: "100%" },
+        },
+        cloud2: {
+          "100%": { right: "0" },
+        },
       },
-      cloud2: {
-        '100%': {right : '0'},
+      animation: {
+        cloud: "cloud 50s linear infinite",
+        cloud2: "cloud2 50s linear infinite",
       },
-    },
-    animation: {
-      cloud: 'cloud 50s linear infinite',
-      cloud2: 'cloud2 50s linear infinite',
-    }
     },
   },
   plugins: [],
-}
+};
